335aa254282ae6ad93c84a85bf8861673b960330
DanielElisenberg <daniel@elisenberg.com>
2022-04-22 17:25:33 +0200

    improvements_job_service: iso date and split collections

--- job_service/repository/job_db.py
+++ job_service/repository/job_db.py
@@ -16,7 +16,8 @@ MONGODB_PASSWORD = os.environ["MONGODB_PASSWORD"]
 
 
 class JobDb:
-    db = None
+    completed = None
+    in_progress = None
 
     def __init__(self):
         client = MongoClient(
@@ -24,17 +25,24 @@ class JobDb:
             username=MONGODB_USER,
             password=MONGODB_PASSWORD
         )
-        self.db = client.jobdb
+        db = client.jobdb
+        self.completed = db.completed
+        self.in_progress = db.inProgress
 
     def get_job(self, job_id: str) -> dict:
         """
         Returns job with matching job_id from database.
         Raises NotFoundException if no such job is found.
         """
-        job = self.db.jobs.find_one({"jobId": job_id})
-        if job is None:
+        results = [
+            self.in_progress.find_one({"jobId": job_id}),
+            self.completed.find_one({"jobId": job_id})
+        ]
+        results = [result for result in results if result is not None]
+        if not results:
             raise NotFoundException(f"No job found for jobId: {job_id}")
 
+        job = results[0]
         del job["_id"]
         self.__stringify_datetime_logs(job["logs"])
         return job
@@ -43,15 +51,18 @@ class JobDb:
         """
         Returns list of jobs with matching status from database.
         """
-        db_query = {} if status is None else {"status": status}
-        jobs = list(self.db.jobs.find(db_query))
+        find_query = {} if status is None else {"status": status}
+        jobs = (
+            list(self.in_progress.find(find_query)) +
+            list(self.completed.find(find_query))
+        )
         for job in jobs:
             del job["_id"]
             self.__stringify_datetime_logs(job["logs"])
         return jobs
 
     def new_job(self, command: str, status: str,
-                dataset_name: str = None) -> str:
+                dataset_name: str = None, log: str = None) -> str:
         """
         Creates a new job for supplied command, status and dataset_name, and
         returns job_id of created job.
@@ -70,26 +81,26 @@ class JobDb:
         }
         if dataset_name is not None:
             job["datasetName"] = dataset_name
+        if log is not None:
+            job["logs"].append({"at": now, "message": log})
+
         if command == "ADD_DATA":
             # Atomic insert of new import job
             # Only inserts if no job is found for same datasetname
             # with a currently active status
-            update_result: UpdateResult = self.db.jobs.update_one(
-                {
-                    "datasetName": dataset_name,
-                    "status": {"$nin": ["done", "failed"]}
-                },
+            update_result: UpdateResult = self.in_progress.update_one(
+                {"datasetName": dataset_name},
                 {"$setOnInsert": job},
                 upsert=True
             )
 
             if update_result.upserted_id is None:
                 # Raise exception if active job already existed
-                raise JobExistsException(f"{dataset_name} already imported")
+                raise JobExistsException(f"{dataset_name} already in progress")
             else:
                 return job_id
         else:
-            self.db.jobs.insert_one(job)
+            self.completed.insert_one(job)
             return job_id
 
     def update_job(self, job_id: str, status: str, log: str = None) -> None:
@@ -97,33 +108,35 @@ class JobDb:
         Updates job with supplied job_id with new status.
         Appends additional log if supplied.
         """
-        self.db.jobs.update_one(
-            {"jobId": job_id},
-            {"$set": {"status": status}}
-        )
         now = datetime.now()
-        self.db.jobs.update_one(
-            {
-                "jobId": job_id
-            },
-            {
-                "$push": {
-                    "log": {"at": now, "message": f"Set status: {status}"}
-                }
+        find_query = {"jobId": job_id}
+        update_status_query = {
+            "$set": {"status": status},
+            "$push": {
+                "log": {"at": now, "message": f"Set status: {status}"}
             }
-        )
+        }
+        add_log_query = {
+            "$push": {
+                "logs": {"at": now, "message": log}
+            }
+        }
+        job = self.in_progress.find_one(find_query)
+        if job is None:
+            raise NotFoundException(f"Could not find job with id {job_id}")
+
+        if status in ["done", "failed"]:
+            self.in_progress.delete_one(find_query)
+            self.completed.insert_one(job)
+            self.completed.update_one(find_query, update_status_query)
+            if log is not None:
+                self.completed.update_one(find_query, add_log_query)
+            return
+
+        self.in_progress.update_one(find_query, update_status_query)
         if log is not None:
-            self.db.jobs.update_one(
-                {
-                    "jobId": job_id
-                },
-                {
-                    "$push": {
-                        "logs": {"at": now, "message": log}
-                    }
-                }
-            )
+            self.in_progress.update_one(find_query, add_log_query)
 
     def __stringify_datetime_logs(self, logs):
         for log in logs:
-            log["at"] = log["at"].strftime("%Y/%m/%d, %H:%M:%S")
+            log["at"] = log["at"].isoformat()
