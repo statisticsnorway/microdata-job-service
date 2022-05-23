79b4af6eeb45a222eb4d05dde3bc88aab7fefad8
Kalantzakos, Vassilios <Vassilios.Kalantzakos@ssb.no>
2022-05-23 11:20:36 +0200

    Rearranged failing tests.

--- test/repository/test_local_storage.py
+++ test/repository/test_local_storage.py
@@ -1,16 +1,19 @@
 from job_service.repository import local_storage
 
-expected_input_datasets = [{'command': 'ADD_OR_CHANGE_DATA', 'datasetName': 'MY_DATASET'},
-                           {'command': 'PATCH_METADATA', 'datasetName': 'YOUR_DATASET'},
-                           {'command': 'ADD_OR_CHANGE_DATA', 'datasetName': 'OTHER_DATASET'}]
+expected_datasets = [{'command': 'ADD_OR_CHANGE_DATA', 'datasetName': 'MY_DATASET'},
+                     {'command': 'PATCH_METADATA', 'datasetName': 'YOUR_DATASET'},
+                     {'command': 'ADD_OR_CHANGE_DATA', 'datasetName': 'OTHER_DATASET'}]
 
 
 def test_get_input_datasets():
-    assert local_storage.get_input_datasets() == expected_input_datasets
+    actual_datasets = local_storage.get_input_datasets()
+    assert len(actual_datasets) == 3
+    for dataset in expected_datasets:
+        assert dataset in actual_datasets
 
 
 def test_has_input_dataset():
-    for dataset in expected_input_datasets:
+    for dataset in expected_datasets:
         has_dataset = local_storage.has_input_dataset(dataset['datasetName'])
         assert has_dataset
 
