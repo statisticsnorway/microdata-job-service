070ba1ae763d472a6cd3c789510c6ac858a93697
DanielElisenberg <daniel@elisenberg.com>
2022-05-27 15:58:35 +0200

    job_service_refactoring: update request model

--- job_service/api/request_models.py
+++ job_service/api/request_models.py
@@ -1,15 +1,15 @@
 from enum import Enum
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import BaseModel, Extra, root_validator
 
 from job_service.exceptions.exceptions import BadRequestException
 
 
-class CommandEnum(str, Enum):
+class OperationEnum(str, Enum):
     ADD_OR_CHANGE_DATA = 'ADD_OR_CHANGE_DATA'
-    REMOVE_DATASET = 'REMOVE'
     PATCH_METADATA = 'PATCH_METADATA'
+    REMOVE_DATASET = 'REMOVE'
     BUMP_VERSION = 'BUMP_VERSION'
     SET_STATUS = 'SET_STATUS'
 
@@ -28,19 +28,20 @@ class StatusEnum(str, Enum):
 
 
 class NewJobRequest(BaseModel, extra=Extra.forbid):
-    command: CommandEnum
+    operation: OperationEnum
     status: StatusEnum
     datasetName: Optional[str]
 
     @root_validator(skip_on_failure=True)
     def check_command_type(cls, values):
-        command = values['command']
-        if command in ['ADD_OR_CHANGE_DATA', 'REMOVE', 'PATCH_METADATA']:
+        operation = values['operation']
+        if operation in ['SET_STATUS', 'ADD_OR_CHANGE_DATA', 'PATCH_METADATA']:
             if values.get('datasetName') is None:
                 raise BadRequestException(
-                    f'Must provide a datasetName when command is {command}.'
+                    'Must provide a datasetName when '
+                    f'operation is {operation}.'
                 )
-        if command != 'ADD_OR_CHANGE_DATA':
+        if operation not in ['PATCH_METADATA', 'ADD_OR_CHANGE_DATA']:
             if values.get('status') not in ['done', 'failed']:
                 raise BadRequestException(
                     'Unable to set status of synchronous job to in progress'
@@ -57,5 +58,10 @@ class GetJobRequest(BaseModel, extra=Extra.forbid):
     status: Optional[str]
 
 
+class ImportableDataset(BaseModel, extra=Extra.forbid):
+    datasetName: str
+    operation: str
+
+
 class ImportRequest(BaseModel, extra=Extra.forbid):
-    datasetList: list
+    importableDatasets: List[ImportableDataset]
