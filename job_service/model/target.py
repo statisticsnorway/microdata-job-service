0d328611e5739af47efcb5e8fe0a31a422cc42f7
DanielElisenberg <Daniel@Elisenberg.com>
2023-03-03 15:04:09 +0100

    targets_collection: update target model

--- job_service/model/target.py
+++ job_service/model/target.py
@@ -1,20 +1,14 @@
 from typing import List
-
 from pydantic import Extra
 
-from job_service.model.enums import JobStatus
 from job_service.model.camelcase_model import CamelModel
+from job_service.model.enums import JobStatus
+from job_service.model.job import UserInfo
 
 
-class UserInfo(CamelModel, extra=Extra.forbid):
-    user_id: str
-    first_name: str
-    last_name: str
-
-
-class Target(CamelModel, use_enum_values=True):
+class Target(CamelModel, use_enum_values=True, extra=Extra.forbid):
     name: str
-    jobs: List[str]
-    last_update: str
+    last_updated_at: str
+    status: JobStatus
     last_updated_by: UserInfo
-    last_status: JobStatus
+    action: List[str]
