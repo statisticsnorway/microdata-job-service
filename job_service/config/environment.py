3ab1f7de12758c5446b55a6f6b8e7390f26a1b62
DanielElisenberg <Daniel@Elisenberg.com>
2023-05-08 15:05:30 +0200

    auth_toggle: auth toggle

--- job_service/config/environment.py
+++ job_service/config/environment.py
@@ -8,7 +8,10 @@ def _initialize_environment() -> dict:
         'JWKS_URL': os.environ['JWKS_URL'],
         'SECRETS_FILE': os.environ['SECRETS_FILE'],
         'DOCKER_HOST_NAME': os.environ['DOCKER_HOST_NAME'],
-        'STACK': os.environ['STACK']
+        'STACK': os.environ['STACK'],
+        'JWT_AUTH': (
+            False if os.environ.get('JWT_AUTH', 'true') == 'false' else True
+        )
     }
 
 
